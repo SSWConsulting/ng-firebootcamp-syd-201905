1e7bf70ba67fc33098c03215ad1f2665d3c8741c
brendan richards <brendan_richards_msdn@hotmail.com>
2019-05-23 16:23:56 +1000

    added presentational component

---
+++ src/app/company/company-table/company-table.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angular/core';
+import { Company } from '../company';
+
+@Component({
+  selector: 'fbc-company-table',
+  templateUrl: './company-table.component.html',
+  styleUrls: ['./company-table.component.scss']
+})
+export class CompanyTableComponent implements OnInit {
+
+  @Input()
+  companies: Company[];
+
+  @Output()
+  deleteClicked: EventEmitter<Company> = new EventEmitter();
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  deleteCompany(company: Company) {
+    this.deleteClicked.emit(company);
+  }
+
+}
