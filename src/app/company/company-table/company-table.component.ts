c6f9695e89ed76be3703fb61574f7e95ac89ef73
brendan richards <brendan_richards_msdn@hotmail.com>
2019-05-23 16:59:05 +1000

    added ngonchanges example

--- src/app/company/company-table/company-table.component.ts
+++ src/app/company/company-table/company-table.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, Output, EventEmitter, ChangeDetectionStrategy, SimpleChanges } from '@angular/core';
 import { Company } from '../company';
 
 @Component({
   selector: 'fbc-company-table',
   templateUrl: './company-table.component.html',
-  styleUrls: ['./company-table.component.scss']
+  styleUrls: ['./company-table.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CompanyTableComponent implements OnInit {
+export class CompanyTableComponent implements OnInit, OnChanges {
 
   @Input()
   companies: Company[];
@@ -21,8 +22,17 @@ export class CompanyTableComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('NG CHANGES', changes);
+
+  }
+
   deleteCompany(company: Company) {
     this.deleteClicked.emit(company);
+  }
+
+  changeDetected() {
+    console.log('CHANGE DETECTED!!');
   }
 
 }
