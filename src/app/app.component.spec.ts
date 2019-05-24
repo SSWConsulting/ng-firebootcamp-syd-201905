ba8f23dc5b3e17d8a46542057185274439c3c794
brendan richards <brendan_richards_msdn@hotmail.com>
2019-05-24 17:00:45 +1000

    testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,107 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { of } from 'rxjs';
+import { componentNeedsResolution } from '@angular/core/src/metadata/resource_loading';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CompanyService } from './company/company.service';
+import { DebugElement } from '@angular/core';
+import { CompanyListComponent } from './company/company-list/company-list.component';
+import { CompanyTableComponent } from './company/company-table/company-table.component';
+import { CompanyEditComponent } from './company/company-edit/company-edit.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { APP_BASE_HREF } from '@angular/common';
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+describe('checking tetsing', () => {
+  it('should add 1+1', () => {
+    expect(1 + 1).toBe(2);
   });
 
-  it(`should have as title 'firebootcamp-crm'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('firebootcamp-crm');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to firebootcamp-crm!');
+  it('can create an appComponent', () => {
+    let c = new AppComponent(null);
+    expect(c.title).toBe('firebootcamp-crm');
   });
 });
+
+
+describe('Mock Service', () => {
+
+  let svc;
+
+  beforeEach(() => {
+    svc = {
+      getCompanies: () => of([{
+        name: 'FakeCorp',
+        email: 'fake@corp.com',
+        phone: 12345
+      }])
+    };
+  });
+
+  it('company count contains 1', () => {
+    let component = new AppComponent(svc);
+    component.ngOnInit();
+    component.companiesCount$.subscribe(cc => expect(cc).toBe(1));
+  });
+
+  it('companyCoiunt contains 2', () => {
+    spyOn(svc, 'getCompanies').and.returnValue(
+      of([
+        { name: 'aaa', phone: 123, email: 'sss@ss.com' },
+        { name: 'bbb', phone: 123, email: 'sss@ss.com' }
+      ])
+    );
+    let component = new AppComponent(svc);
+    component.ngOnInit();
+    component.companiesCount$.subscribe(cc => expect(cc).toBe(2));
+  });
+
+});
+
+
+describe('testBed', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+  let companySvc: CompanyService;
+  let de: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent,
+        CompanyListComponent,   // Our routing module needs it
+        CompanyTableComponent,  // Our routing module needs it
+        CompanyEditComponent,   // Our routing module needs it
+      ],
+      imports: [
+        AppRoutingModule, // Routerlink in AppComponent needs it
+        HttpClientModule,
+        ReactiveFormsModule
+      ],
+      providers: [{provide: APP_BASE_HREF, useValue: '/'}]
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    companySvc = TestBed.get(CompanyService);
+  });
+
+  it('companyCount is 2', () => {
+
+    spyOn(companySvc, 'getCompanies').and.returnValue(
+      of([
+        { name: 'aaa', phone: 123, email: 'sss@ss.com' },
+        { name: 'bbb', phone: 123, email: 'sss@ss.com' }
+      ])
+    );
+    fixture.detectChanges();
+
+    expect(component.companiesCount$.subscribe(cc => {
+      expect(cc).toEqual(2);
+    }));
+
+  });
+});
+
+
+
